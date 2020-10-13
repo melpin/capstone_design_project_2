2017d4fae3770f77eb346b9ea78d3dac39699c93
hyunjin072 <47468512+hyunjin072@users.noreply.github.com>
2020-10-13 23:01:09 +0900

    Add files via upload

--- richheader.py
+++ richheader.py
@@ -1,33 +1,65 @@
 #!/usr/bin/env python3
-#출처 : https://github.com/HolmesProcessing/RichHeader-Service_Collection
-#참조
-#https://infocon.hackingand.coffee/Hacktivity/Hacktivity%202016/Presentations/George_Webster-and-Julian-Kirsch.pdf?__cf_chl_jschl_tk__=365c68d8b1d8892dd6ffcfeb05c4a6d0043ed0ad-1600313813-0-AUDiL9fOGEOL0XmW7bHoxdqrNTOBHBJkyG-r43P2z7ZAIFyns2VtkjY0bVRyeXKO0jljtMFkoQYAc7mjtPC2-NJRnq1VF0gZVTS_2BjpA4BXgP18TEzjAq-PZPfP310AU7iE55QKFpYIbiOaXBVghBA_4QDLfxg3HGuUQ3iZjVRCVou0qLz1hnBKyP_181uh78Wa6AoLYSWMT5GJGkegwIaOz632VnsW1ubNfAWGlZpfI6r6XmwRMK1ctz58gdYEoNrMfYHrYc2CNse9_DVEokIXPc4beAHxw6SOYpGjaXIyZ1aKBv8z2h6MlAtlGnsGk_3xcC8VDrLQ-aHxWrkeQCdWKk_LbRkn2LpaBaSaYx2apz1oaXIPl6fAepRONyL7Zw
-#https://www.youtube.com/watch?v=ftZQGDujLgo
-#https://www.sans.org/reading-room/whitepapers/reverseengineeringmalware/paper/39045
-#https://www.virusbulletin.com/virusbulletin/2020/01/vb2019-paper-rich-headers-leveraging-mysterious-artifact-pe-format/#ref13
+# 출처 : https://github.com/HolmesProcessing/RichHeader-Service_Collection
+# 참조
+# https://infocon.hackingand.coffee/Hacktivity/Hacktivity%202016/Presentations/George_Webster-and-Julian-Kirsch.pdf?__cf_chl_jschl_tk__=365c68d8b1d8892dd6ffcfeb05c4a6d0043ed0ad-1600313813-0-AUDiL9fOGEOL0XmW7bHoxdqrNTOBHBJkyG-r43P2z7ZAIFyns2VtkjY0bVRyeXKO0jljtMFkoQYAc7mjtPC2-NJRnq1VF0gZVTS_2BjpA4BXgP18TEzjAq-PZPfP310AU7iE55QKFpYIbiOaXBVghBA_4QDLfxg3HGuUQ3iZjVRCVou0qLz1hnBKyP_181uh78Wa6AoLYSWMT5GJGkegwIaOz632VnsW1ubNfAWGlZpfI6r6XmwRMK1ctz58gdYEoNrMfYHrYc2CNse9_DVEokIXPc4beAHxw6SOYpGjaXIyZ1aKBv8z2h6MlAtlGnsGk_3xcC8VDrLQ-aHxWrkeQCdWKk_LbRkn2LpaBaSaYx2apz1oaXIPl6fAepRONyL7Zw
+# https://www.youtube.com/watch?v=ftZQGDujLgo
+# https://www.sans.org/reading-room/whitepapers/reverseengineeringmalware/paper/39045
+# https://www.virusbulletin.com/virusbulletin/2020/01/vb2019-paper-rich-headers-leveraging-mysterious-artifact-pe-format/#ref13
 
-import sys
+import csv
+import hashlib
+import operator
+import os
 import traceback
+from itertools import chain
 
 # imports for rich
 import richlibrary
 
 
-file = "C:\\WINDOWS\\system32\\notepad.exe"
-sys.argv = ["richheader.py", file]
+class RichHeader_features:
+    def __init__(self, output):
+        self.output = output
+        self.rich = dict()
+        self.imports = ""
 
+    def richs(self, rich_g, ex_mode):
+        if ex_mode == 1:            
+            rich = self.rich
+        elif ex_mode == 0:
+            rich = dict()
 
-def RichHeader(objpath):
-    return richlibrary.RichLibrary(objpath)
+        if(rich_g['error'] < 0):
+            rich['error'] = rich_g['error']
+            return rich
 
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: {} <pe-files>".format(sys.argv[0]))
-        sys.exit(-1)
-    for arg in sys.argv[1:]:
+        cmpids = rich_g['cmpids']
+
+        for lis in cmpids:
+            try:
+                rich[lis['pid']] += lis['cnt']
+            except:
+                rich[lis['pid']] = lis['cnt']
+        return rich
+
+    def get_rich_count(self, headers, rich, label):
+        pids = list()
+
+        for pid in headers:
+            try:
+                pids.append(rich[pid])
+            except:
+                pids.append(0)
+
+        pids.append(label)
+
+        return pids
+
+    def get_rich_parser(selfself, file):
+
+        rich_parser = richlibrary.RichLibrary(file)
         error = 0
-        rich_parser = RichHeader(arg)
-
+        rich = {'error': 0, 'cmpids': [{'mcv': 0, 'pid': 0, 'cnt': 0}], 'csum_calc': 0, 'csum_file': 0, 'offset': 0}
         try:
             rich = rich_parser.parse()
         except richlibrary.FileSizeError:
@@ -48,21 +80,113 @@ def main():
             error = -9
         except Exception as e:
             print(traceback.format_exc(e))
+        rich['error'] = error
 
-        #if error < 0:
-        #    print("\x1b[33m[-] " + richlibrary.err2str(error) + "\x1b[39m")
-        #    sys.exit(error)
-        #else:
-        #    rich_parser.pprint_header(rich)    
-        #print(rich)
-        if rich['error'] == -6:                     # RichHeader Not Found
-            return 0
-        elif rich['csum_calc'] != rich['csum_file']:# Checksum Not Equal
-            return -1
-        else :                                      # etc
-            return rich['cmpids']
-#etc부분 고민좀 해봐야 할거같음..
-#단순 PID 뿐만 아니라 Count까지 비교해서 악성여부를 판단하기 때문..
+        return rich
+
+    def getMD5(self, filepath):
+        with open(filepath, 'rb') as fh:
+            m = hashlib.md5()
+            while True:
+                data = fh.read(8192)
+                if not data:
+                    break
+                m.update(data)
+            return m.hexdigest()
+
+    def write_csv_header(self, headers):
+        filepath = self.output
+        HASH = ['filename', 'MD5']
+        class_ = ['class']
+        headers = HASH + headers + class_
+
+        csv_file = open(filepath, "w+")
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(headers)
+        csv_file.close()
+
+    def write_csv_data(self, data):
+        filepath = self.output
+        csv_file = open(filepath, "a")
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(data)
+        csv_file.close()
+
+
+def main():
+    mal_path = './samples/malware/'
+    nor_path = './samples/normal/'
+    output_file = "./richd.csv"
+
+    print('[*] Extracting ngram patterns from files')
+
+    ef = RichHeader_features(output_file)
+    i = 0
+
+    for file in os.listdir(mal_path):
+        i += 1
+        print("%d file processd (%s), " % (i, file))
+        file = mal_path + file
+        rich_g = ef.get_rich_parser(file)
+
+        richs = ef.richs(rich_g, 1)
+        print("%d rich_pids extracted" % (len(richs)))
+
+    print("- Malware Completed")
+    print(richs)
+
+    for file in os.listdir(nor_path):
+        i += 1
+        print("%d file processd (%s), " % (i, file))
+        # file = nor_path + file
+        file = "C:\\WINDOWS\\system32\\notepad.exe"
+        rich_g = ef.get_rich_parser(file)
+
+        richs = ef.richs(rich_g, 1)
+        print("%d rich_pids extracted" % (len(richs)))
+
+    print("- Normal Complated")
+    print(richs)
+    print("[*] Total length of rich_pid list :", len(richs))
+    num_of_features = len(richs)
+
+    sorted_x = sorted(richs.items(), key=operator.itemgetter(1), reverse=True)
+    print("[*] Using %s richs as features" % num_of_features)
+    features = sorted_x[0:num_of_features]
+    headers = list(chain.from_iterable(zip(*features)))[0:num_of_features]
+    ef.write_csv_header(headers)
+    print(features)
+    print("#" * 80)
+
+    i = 0
+
+    for file in os.listdir(mal_path):
+        i += 1
+        print("%d file processed (%s), " % (i, file))
+        filepath = mal_path + file
+        rich_c = ef.get_rich_parser(filepath)
+        richs = ef.richs(rich_c, 0)
+
+        rich_count = ef.get_rich_count(headers, richs, 1)
+        hash_ = ef.getMD5(filepath)
+        all_data = [file, hash_]
+        all_data.extend(rich_count)
+        ef.write_csv_data(all_data)
+
+    for file in os.listdir(nor_path):
+        i += 1
+        print("%d file processed (%s), " % (i, file))
+        # filepath = nor_path + file
+        filepath = "C:\\WINDOWS\\system32\\notepad.exe"
+        rich_c = ef.get_rich_parser(filepath)
+        richs = ef.richs(rich_c, 0)
+
+        rich_count = ef.get_rich_count(headers, richs, 0)
+        hash_ = ef.getMD5(filepath)
+        all_data = [file, hash_]
+        all_data.extend(rich_count)
+        ef.write_csv_data(all_data)
+
 
 if __name__ == '__main__':
     main()
