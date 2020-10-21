1b3a557dba9ae92f2eccba9668122f3e448cb659
melpin <hwangju7476@naver.com>
2020-10-22 04:59:56 +0900

    peid detector modify

--- engine/PyPackerDetect/PEIDDetector.py
+++ engine/PyPackerDetect/PEIDDetector.py
@@ -10,13 +10,16 @@ class PEIDDetector(PackerDetector):
     def __init__(self, config):
         super().__init__(config)
         
-        try:
+         try:
+            if (self.config["UseLargePEIDDatabase"]):
+                self.signatures = peutils.SignatureDatabase('deps/peid/signatures_long.txt')
+            else:
+                self.signatures = peutils.SignatureDatabase('deps/peid/signatures_short.txt')
+        except:
             if (self.config["UseLargePEIDDatabase"]):
                 self.signatures = peutils.SignatureDatabase(PyPackerDetecter_path+'\\deps\\peid\\signatures_long.txt')
             else:
                 self.signatures = peutils.SignatureDatabase(PyPackerDetecter_path+'\\deps\\peid\\signatures_short.txt')
-        except:
-            print("error")
 
     def Run(self, pe, report):
         if (not self.config["CheckForPEIDSignatures"]):
